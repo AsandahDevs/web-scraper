2de8cdf30c51b5325622e2a955991bebeec419b1
Asanda <noelmajola@gmail.com>
2024-09-07 23:45:45 +0200

    feat : add scraper for TOPS SEADOONE specials

--- retail_scrapers/spar_tops_liquor_scraper.py
+++ retail_scrapers/spar_tops_liquor_scraper.py
@@ -1,2 +1,36 @@
 # This script scrapes all liquor deals at Tops liquor stores (nation wide)
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class Spar(webdriver.Chrome):
+    def __init__(self):
+        super().__init__()
+
+    def open_spar_liquor_site(self):
+        '''
+        opens spar liquor site and views specials
+        available at toti durban branch
+        '''
+        BROWSER_CLICK_EVENT = 'arguments[0].click();'
+
+        self.get('https://www.topsatspar.co.za/Home#')
+        self.implicitly_wait(30)
+        verification_button = self.find_element(By.XPATH,value='//*[@id="AGModal"]/div/div/div/a[1]')
+        self.execute_script(BROWSER_CLICK_EVENT, verification_button)
+        self.implicitly_wait(30)
+        specials_link = self.find_element(By.CSS_SELECTOR, "a.mega-menu[href='/Stores/Store-Finder?specials=1']")
+        self.execute_script(BROWSER_CLICK_EVENT, specials_link)
+        self.implicitly_wait(15)
+        search_field = self.find_element(By.XPATH,value='//*[@id="searchTerm"]')
+        self.execute_script(BROWSER_CLICK_EVENT, search_field)
+        search_field.send_keys('toti')
+        search_icon = self.find_element(By.XPATH,value='//*[@id="StoreFinderApp"]/div[2]/div[1]/div[1]/div[3]/a')
+        print(search_icon)
+        self.execute_script(BROWSER_CLICK_EVENT, search_icon)
+        self.implicitly_wait(30)
+        toti_durban_area = self.find_element(By.XPATH,value='//*[@id="allStores"]/a[2]/div/div/a')
+        self.execute_script(BROWSER_CLICK_EVENT, toti_durban_area)
+        self.implicitly_wait(30)
+        name_of_liquor_store = self.find_element(By.ID,value='storeName').text
+        print(name_of_liquor_store)
+        self.close()
