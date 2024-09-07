a8ab37d7c2de1eea2181103ee6304f817dad9ff3
Asanda <noelmajola@gmail.com>
2024-09-07 21:38:24 +0200

    update : create empty function to filtering cars on auto-trader

--- automotive_scrapers/autotrader_scraper.py
+++ automotive_scrapers/autotrader_scraper.py
@@ -1,15 +1,21 @@
-#This script automates the searching of vehicles on the AutoTrader website
+# This script automates the searching of vehicles on the AutoTrader website
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-# TODO: search_vw_gtis_that_have_mileage_less_than_100000_km # type: ignore
+
 
 class AutoTrader(webdriver.Chrome):
+
     def open_autotrader_site(self):
         browser = webdriver.Chrome()
-        browser.get('https://www.autotrader.co.za/')
+        browser.get("https://www.autotrader.co.za/")
         browser.fullscreen_window()
         browser.refresh()
         browser.implicitly_wait(15)
         browser.quit()
 
+    def view_gtis_from_year_2015_and_upwards(self):
+        """
+        this function will get vw golf gtis from the year 2015 and onwards
+        """
+        pass
